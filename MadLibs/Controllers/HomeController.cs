d27028e239398aaabe990de7f06bd3b421025fc3
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-18 10:02:54 -0700

    Allow you to play madlibs in view

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Madlib.Controllers
     [Route("/")]
     public ActionResult Letter()
     {
-     MadVariable myMadlib = new MadVariable();
+      MadVariable myMadlib = new MadVariable();
       myMadlib.noun1 = "noun1";
       myMadlib.noun2 = "noun2";
       myMadlib.verb1 = "verb1";
@@ -21,7 +21,7 @@ namespace Madlib.Controllers
     public ActionResult Form() { return View(); }
 
     [Route("/madlib")]
-    public ActionResult Postcard(string noun1, string noun2, string verb1, string verb2)
+    public ActionResult Madlib(string noun1, string noun2, string verb1, string verb2)
     {
       MadVariable myMadlib = new MadVariable();
       myMadlib.noun1 = noun1;
