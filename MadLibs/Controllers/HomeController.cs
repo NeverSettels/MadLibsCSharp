c79ebbd66ad0843bedecad2d18a46c7d2229079f
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-18 10:44:59 -0700

    Display a madlib let you populate it and has weblinks to iterate through the site

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -7,27 +7,18 @@ namespace Madlib.Controllers
   {
 
     [Route("/")]
-    public ActionResult Letter()
+    public ActionResult Launch()
     {
-      MadVariable myMadlib = new MadVariable();
-      myMadlib.noun1 = "noun1";
-      myMadlib.noun2 = "noun2";
-      myMadlib.verb1 = "verb1";
-      myMadlib.verb2 = "verb2";
-      return View(myMadlib);
+      return View();
     }
 
     [Route("/form")]
     public ActionResult Form() { return View(); }
 
     [Route("/madlib")]
-    public ActionResult Madlib(string noun1, string noun2, string verb1, string verb2)
+    public ActionResult Madlib(string properNoun, string noun2, string verb1, string verb2)
     {
-      MadVariable myMadlib = new MadVariable();
-      myMadlib.noun1 = noun1;
-      myMadlib.noun2 = noun2;
-      myMadlib.verb1 = verb1;
-      myMadlib.verb2 = verb2;
+      MadVariable myMadlib = new MadVariable(properNoun, noun2, verb1, verb2);
       return View(myMadlib);
     }
 
--- MadLibs/Models/MadLibVariables.cs
+++ MadLibs/Models/MadLibVariables.cs
@@ -2,9 +2,16 @@ namespace Madlib.models
 {
   public class MadVariable
   {
-    public string noun1 { get; set; }
-    public string noun2 { get; set; }
-    public string verb1 { get; set; }
-    public string verb2 { get; set; }
+    public string Noun1 { get; set; }
+    public string Noun2 { get; set; }
+    public string Verb1 { get; set; }
+    public string Verb2 { get; set; }
+    public MadVariable(string noun1, string noun2, string verb1, string verb2)
+    {
+      Noun1 = noun1;
+      Noun2 = noun2;
+      Verb1 = verb1;
+      Verb2 = verb2;
+    }
   }
 }
