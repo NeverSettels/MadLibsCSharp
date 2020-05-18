abe9dffaaa2711f7c1d54c2d94b5f207c32835b3
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-18 09:49:26 -0700

    initial

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Madlib.models;
+
+namespace MAdlib.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/")]
+    public ActionResult Letter()
+    {
+     MadVariable myMadlib = new MadVariable();
+      myMadlib.noun1 = "noun1";
+      myMadlib.noun2 = "noun2";
+      myMadlib.verb1 = "verb1";
+      myMadlib.verb2 = "verb2";
+      return View(myMadlib);
+    }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/madlib")]
+    public ActionResult Postcard(string noun1, string noun2, string verb1, string verb2)
+    {
+      MadVariable myMadlib = new MadVariable();
+      myMadlib.noun1 = noun1;
+      myMadlib.noun2 = noun2;
+      myMadlib.verb1 = verb1;
+      myMadlib.verb2 = verb2;
+      return View(myMadlib);
+    }
+
+  }
+}
---
+++ MadLibs/Models/MadLibVariables.cs
@@ -0,0 +1,10 @@
+namespace Madlib.models
+{
+  public class MadVariable
+  {
+    public string noun1 { get; set; }
+    public string noun2 { get; set; }
+    public string verb1 { get; set; }
+    public string verb2 { get; set; }
+  }
+}
